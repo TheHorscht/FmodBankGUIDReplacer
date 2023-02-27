f015e9fdb3c8e6bbdd4d7e5b16a091f0cdbb5686
The Horscht <realhorscht+github@gmail.com>
2023-02-27 23:25:51 +0100

    Add readme and format usage

---
+++ README.md
@@ -0,0 +1,11 @@
+### Usage:
+
+`guid_replacer.exe your_audio.bank [GUID to replace] [what to replace it with]`
+
+### Example:
+
+`guid_replacer.exe your_audio.bank {bc5e80f9-a380-47c9-8799-3e02eea94feb} {aabbccdd-eeff-00ff-aabb-feffefbbaacc}`
+
+Multiple replacements can be chained like this:
+
+`guid_replacer.exe your_audio.bank {GUID_A1} {GUID_A2} {GUID_B1} {GUID_B2}`
--- main.cpp
+++ main.cpp
@@ -162,7 +162,7 @@ int wrongUsage() {
   cerr << "Usage: guid_replacer.exe your_audio.bank [GUID to replace] [what to replace it with]" << endl;
   cerr << "Example: guid_replacer.exe your_audio.bank {bc5e80f9-a380-47c9-8799-3e02eea94feb} {aabbccdd-eeff-00ff-aabb-feffefbbaacc}" << endl;
   cerr << "Multiple replacements can be chained like this:" << endl;
-  cerr << "guid_replacer.exe your_audio.bank GUID_A1 GUID_A2 GUID_B1 GUID_B2" << endl;
+  cerr << "guid_replacer.exe your_audio.bank {GUID_A1} {GUID_A2} {GUID_B1} {GUID_B2}" << endl;
   return -1;
 }
 
